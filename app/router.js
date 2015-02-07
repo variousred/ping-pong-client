a35cf1ecc52a1687bb27e93ea04039f52972b438
Michael Mitchell <michaelmitchell@gmail.com>
2015-02-07 08:08:10 -0800

    adds player show route, links to players in players index

--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ var Router = Ember.Router.extend({
 Router.map(function() {
   this.resource('players', function(){
     this.route("new");
+    this.route("show", { path: ":player_id" });
   });
 });
 
